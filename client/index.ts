b5352967fe4c591f4f7f1de3e587694ffed4b5b5
walze <wallacefares@gmail.com>
2022-09-30 15:17:42 -0300

    feat: added ws

--- client/index.ts
+++ client/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+export const socket = new WebSocket("ws://localhost:1234");
+
+socket.addEventListener("open", () => {
+  console.log("connected");
+
+  socket.send("something");
+});
+
+socket.addEventListener("message", (event) => {
+  console.log("received: %s", event.data);
+});
--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,17 @@
-console.log(123);
+import { createServer } from "http";
+import { WebSocketServer } from "ws";
+
+const server = createServer();
+server.listen(1234);
+
+const ws = new WebSocketServer({ server });
+
+ws.on("connection", (client) => {
+  console.log("connected");
+
+  client.on("message", (data) => {
+    console.log("received: %s", data);
+  });
+
+  client.send("something else");
+});
