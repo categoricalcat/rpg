76836a2886fdc5036444c74c081b919c9562f683
walze <wallacefares@gmail.com>
2022-10-01 15:37:08 -0300

    refactor: server fn

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,7 @@
-import { createReadStream } from "fs";
-import type { ServerResponse } from "http";
 import { createServer } from "https";
-import mime from "mime";
-import path from "path";
 import { WebSocketServer } from "ws";
 import cert from "../cert";
-
-const publicF = path.join(__dirname, "../docs");
+import serve from "./serve";
 
 const port = 9876;
 export const origin =
@@ -14,37 +9,16 @@ export const origin =
     ? "https://walze.github.io/rpg"
     : "http://localhost:6789";
 
-const server = createServer({ ...cert });
+const server = createServer(cert);
 
-server.listen(port, () => {
-  console.log(`listening ==> https://0.0.0.0:${port}`);
-});
-
-const serveFile = (url: string, res: ServerResponse) => {
-  const type = mime.getType(url);
-
-  if (!type) throw "no type";
-
-  const stream = createReadStream(publicF + url);
-
-  stream.on("error", () => {
-    res.statusCode = 404;
-    res.end();
-  });
-
-  stream.on("open", () => {
-    res.writeHead(200, { "Content-Type": type });
-    stream.pipe(res);
-  });
-};
+server.listen(port, () => console.log(`listening ==> https://0.0.0.0:${port}`));
 
 server.on("request", (req, res) => {
   const url = !req.url?.includes(".") ? `${req.url}/index.html` : req.url;
-
-  if (!url) throw 1;
+  if (!url) throw "no url";
 
   try {
-    serveFile(url, res);
+    serve(url, res);
   } catch (e) {
     res.statusCode = 404;
 
