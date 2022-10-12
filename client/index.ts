63ea0a8ada2c70fc8cb0ec94b9ed9220b68e76b8
walze <wallacefares@gmail.com>
2022-10-12 18:13:49 -0300

    feat: started command parsing

--- client/index.ts
+++ client/index.ts
@@ -4,8 +4,31 @@ export const $$ = document.querySelectorAll.bind(document);
 const $input = $("#chat") as HTMLInputElement | null;
 if (!$input) throw new Error("Nao achou input");
 
+class Command {
+  static isCommand(m: string) {
+    return m.startsWith("/");
+  }
+}
+
+class Message {
+  text: string;
+
+  constructor(text: string) {
+    this.text = text.trim();
+  }
+}
+
 $input.addEventListener("keydown", (e) => {
   if (e.code !== "Enter") return;
 
-  alert("enviado");
+  const message = new Message($input.value);
+  const isCommand = Command.isCommand(message.text);
+
+  if (isCommand) {
+    const command = new Command();
+
+    return console.log("command", command);
+  }
+
+  console.log("enviado", message);
 });
