d45d026a5ede1762145c25245307ccd6b435715c
wallace fares <wallacefares@gmail.com>
2022-11-02 16:11:58 -0300

    aaaaa

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const tailwindcss = {
   content: ['./client/index.html', './client/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
+};
+
+export default {
+  plugins: {
+    tailwindcss,
+  },
 };
