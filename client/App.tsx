94c8cc763c54f35385c4e3e7b5319071691303e5
wallace fares <wallacefares@gmail.com>
2022-11-04 17:26:32 -0300

    chore: css

--- client/App.tsx
+++ client/App.tsx
@@ -17,19 +17,19 @@ export const App = () => {
 
   return (
     <>
-      <h1 className="font-bold text-4xl mb-4">RPG</h1>
+      <h1 className="font-bold text-4xl mt-6">RPG</h1>
 
-      <ul className="w-full mt-16 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:grid-cols-3 lg:gap-x-8">
+      <ul className="mt-6">
         {ms.map((m) => (
-          <li class="mb-4 flex flex-col items-start">
+          <li class="mb-2 flex flex-col items-start even:bg-gray-50 p-4 pb-5">
             <h2
               id="episode-5-title"
-              class="mt-2 text-lg text-slate-900"
+              class="text-lg text-slate-900"
             >
               <span className="font-bold">{m.sender}</span> -{' '}
               <time
                 dateTime="2022-02-24T00:00:00.000Z"
-                class="order-first font-mono text-sm leading-7 text-slate-500"
+                class="order-first font-mono text-sm leading-7 text-slate-400"
               >
                 {new Date(m.createdAt).toLocaleString()}
               </time>
@@ -37,7 +37,7 @@ export const App = () => {
 
             <p class="mt-1 text-base leading-7 text-slate-700">
               {isImage(m.text) ? (
-                <img width={256} src={m.text} />
+                <img width={128} src={m.text} />
               ) : (
                 <p>{m.text}</p>
               )}
