50d8473e48662a20e3fba930360be01b6bac50f9
wallace fares <wallacefares@gmail.com>
2022-11-07 15:11:50 -0300

    feat: db

--- server/db/index.ts
+++ server/db/index.ts
@@ -12,15 +12,38 @@ prisma
   .then(() => console.log('\nConnected to database\n'))
   .catch(console.error);
 
-prisma.user
-  .upsert({
-    create: {
-      name: 'Wallace',
-    },
-    update: {},
-    where: {
-      name: 'Wallace',
-    },
-  })
-  .then(console.log)
-  .catch(console.error);
+// prisma.sheet
+//   .create({
+//     data: {
+//       name: 'Fasfaf F.',
+//       description: 'O senhor do medo...',
+//       attribute: {
+//         set: {
+//           STR: 10,
+//           DEX: 10,
+//           CON: 10,
+//           INT: 10,
+//           WIS: 10,
+//           CHA: 10,
+//           PWR: 10,
+//         },
+//       },
+//       items: {
+//         set: [
+//           {
+//             name: 'Espada',
+//             description: 'Uma espada de aço',
+//             type: 'ITEM',
+//             exertion: '2.6 + 5 + STR',
+//           },
+//           {
+//             name: 'Amedrontar',
+//             description: 'Ameaça o inimigo',
+//             type: 'SPELL',
+//             exertion: '4.6 + PWR',
+//           },
+//         ],
+//       },
+//     },
+//   })
+//   .catch(console.error);
